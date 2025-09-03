3acd08d1b941219d945d7a44aa0089e58ef67ba4
MinhDuc1711 <99374119+MinhDuc1711@users.noreply.github.com>
2025-09-03 11:03:17 -0600

    updated card images, added github link to realestate website

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -10,13 +10,13 @@ function Card() {
             <div className="cards__wrapper">
                 <ul className="cards__items">
                     <CardItem 
-                    src={process.env.PUBLIC_URL + "/images/logo192.png"}
+                    src={process.env.PUBLIC_URL + "/images/condo-photo.jpg"}
                     text="Simulated real estate management website built using Python, REST and Express"
                     label="Real estate website"
                     path='/projects/realestatewebsiteproject'
                     />
                     <CardItem 
-                    src={process.env.PUBLIC_URL + "/images/logo192.png"}
+                    src={process.env.PUBLIC_URL + "/images/unity_logo.png"}
                     text="Gamified solution to urban planning built using Unity 3D and C#"
                     label="Urban planning game"
                     path='/projects/unitygameproject'
--- src/Components/Pages/RealEstateWebsiteProject.js
+++ src/Components/Pages/RealEstateWebsiteProject.js
@@ -9,7 +9,11 @@ function RealEstateWebsiteProject() {
             <h1 className='about'>Real Estate Website Project</h1>
             <div className="cards">
                 <h1>Welcome to my Real Estate Website Project</h1>
-                
+                <h1>
+                    <a href="https://github.com/MinhDuc1711/Real-estatement-management-website" target="_blank" rel="noopener noreferrer">
+                        Check out the Github page
+                    </a>
+                </h1>
             </div>
         </div>
     )
